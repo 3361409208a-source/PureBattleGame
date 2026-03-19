5275bece213bb855b9e933a108f754e6836fb8be
hualuoshi <3361409208@qq.com>
2026-03-19 18:41:03 +0800

    Cleanup temporary scripts

--- fix_braces.ps1
+++
@@ -1,7 +0,0 @@
-$path = "c:\Users\18229\.openclaw\workspace\PureBattleGame\Games\StarCoreDefense\BattleForm.cs"
-$lines = Get-Content $path
-if ($lines[-1] -match "}") {
-    $newLines = $lines[0..($lines.Count - 2)]
-    Set-Content $path $newLines
-    Write-Host "Removed last line"
-}
